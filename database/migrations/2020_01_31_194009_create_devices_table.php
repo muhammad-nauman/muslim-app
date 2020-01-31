062f670746306bd2ea238a4f4f58fe79f1447af2
Muhammad Nauman <naumancs@gmail.com>
2020-02-01 00:43:05 +0500

    wip

---
+++ database/migrations/2020_01_31_194009_create_devices_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDevicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('devices', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('udid')->index();
+            $table->string('fcm_id');
+            $table->string('is_active')->default(1);
+            $table->timestamp('last_login')->nullable();
+            $table->timestamps();
+            $table->softDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('devices');
+    }
+}

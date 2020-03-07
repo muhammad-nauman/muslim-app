4312fd07bb0b58b818be634171f10c20da04e646
Muhammad Nauman <naumancs@outlook.com>
2020-03-08 02:24:58 +0500

    updated migration

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('reminders:publish')->everyTenMinutes();
+        $schedule->command('reminder:publish')->everyTenMinutes();
     }
 
     /**
